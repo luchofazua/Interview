22df12dd583884ea7d63f7669ea8eec75f3bcb8f
Luciano Sorrentino <sorrentino_luciano@hotmail.com>
2019-09-16 19:40:21 +0200

    First screen

---
+++ src/sample/Controller.java
@@ -0,0 +1,11 @@
+package sample;
+
+public class Controller {
+
+
+    public void createAndTestButtonClicked(){
+        //Test every input and move on
+        System.out.println("Validating all values...");
+        System.out.println("Evation unit created and tested");
+    }
+}
